f370da3a0276d1192d68dff35b55a741d277e97e
Andrés Leiva <aleiva1700@gmail.com>
2024-02-18 20:07:18 -0500

    feat(sagas): add saga pattern

--- src/alarms/application/alarms.module.ts
+++ src/alarms/application/alarms.module.ts
@@ -1,13 +1,19 @@
 import { DynamicModule, Module, Type } from '@nestjs/common';
-import { AlarmsService } from './alarms.service';
-import { AlarmsController } from '../presenters/http/alarms.controller';
 import { AlarmFactory } from '../domain/factories/alarm.factory';
+import { AlarmsController } from '../presenters/http/alarms.controller';
+import { AlarmsService } from './alarms.service';
 import { CreateAlarmCommandHandler } from './commands/create-alarm.command-handler';
 import { GetAlarmsQueryHandler } from './queries/get-alarms.query-handler';
 import { AlarmCreatedEventHandler } from './event-handlers/alarm-created.event-handler';
+import { AcknowledgeAlarmCommandHandler } from './commands/acknowledge-alarm.command-handler';
 import { AlarmAcknowledgedEventHandler } from './event-handlers/alarm-acknowledged.event-handler';
+import { CascadingAlarmsSaga } from './sagas/cascading-alarms.saga';
+import { NotifyFacilitySupervisorCommandHandler } from './commands/notify-facility-supervisor.command-handler';
+import { UnacknowledgedAlarmsSaga } from './sagas/unacknowledged-alarms.saga';
+import { SharedModule } from '../../shared/shared.module';
 
 @Module({
+  imports: [SharedModule],
   controllers: [AlarmsController],
   providers: [
     AlarmsService,
@@ -15,7 +21,11 @@ import { AlarmAcknowledgedEventHandler } from './event-handlers/alarm-acknowledg
     CreateAlarmCommandHandler,
     GetAlarmsQueryHandler,
     AlarmCreatedEventHandler,
+    AcknowledgeAlarmCommandHandler,
     AlarmAcknowledgedEventHandler,
+    CascadingAlarmsSaga,
+    NotifyFacilitySupervisorCommandHandler,
+    UnacknowledgedAlarmsSaga,
   ],
 })
 export class AlarmsModule {
